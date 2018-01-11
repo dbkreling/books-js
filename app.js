5423844ef560c8dc3c2dd13a1c65a5b03a2c8984
Daniel Kreling <dbkreling@gmail.com>
2018-01-11 18:24:58 -0200

    app.js: print books on browser
    Make app.js print the ten first entries from the GoogleBooks API to
    the browser, based on the theme subject passed in the URL.
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- app.js
+++ app.js
@@ -3,27 +3,26 @@ const request = require('request');
 var app = express();
 var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+
 app.get('/books', (req, res) => {
+    var theme = req.query.theme;
     request({
-        url: 'https://www.googleapis.com/books/v1/volumes?q=title:java',
+        url: `https://www.googleapis.com/books/v1/volumes?q=title:${theme}`,
         json: true
     }, (err, response, body) => {
-        res.send({
-            title: body.items[0].volumeInfo.title,
-            authors: body.items[0].volumeInfo.authors,
-            ISBN: body.items[0].volumeInfo.industryIdentifiers[0].identifier
-        });
+        var a = [];
+        for (var i = 0; i < 10; i++) {
+            a.push({
+                    title: body.items[i].volumeInfo.title,
+                    authors: body.items[i].volumeInfo.authors,
+                    ISBN: body.items[i].volumeInfo.industryIdentifiers[0].identifier
+                });
+        }
+        res.send(a);
     });
 });
-
 
 app.listen(3000, () => {
     console.log("server running on port 3000");
 });
-
-
-// https://www.googleapis.com/books/v1/volumes?q=title:java
-
-// console.log(`Address: ${body.results[0].formatted_address}`);
-// console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-// console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
